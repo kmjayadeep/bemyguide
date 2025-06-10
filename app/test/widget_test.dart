afd210570af73430cb6d81eb2168a51f2d28658a
Jayadeep KM <kmjayadeep@gmail.com>
2025-06-10 03:59:13 +0200

    fix tests
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:app/main.dart';
+import 'package:bemyguide/main.dart';
 
 void main() {
   testWidgets('BeMyGuide app loads correctly', (WidgetTester tester) async {
