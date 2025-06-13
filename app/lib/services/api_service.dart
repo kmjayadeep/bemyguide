a61d99319bbe4bd52229a8de5a401a8f81128b4e
Jayadeep KM <kmjayadeep@gmail.com>
2025-06-13 07:29:48 +0200

    fix flutter analyze
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- app/lib/services/api_service.dart
+++ app/lib/services/api_service.dart
@@ -3,7 +3,6 @@ import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:device_info_plus/device_info_plus.dart';
 import 'dart:io';
-import 'package:crypto/crypto.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class PlaceSuggestion {
